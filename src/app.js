935f63513d1b73be74926ea1b99d2a6ac96aae92
Gavin King <gavin.king@fronter.com>
2014-09-28 01:39:38 +0200

    Working example

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ angular.module('releaseHeat', ['office.ui'])
     .value('OfficeConfig', [
         {
             'name': 'Ben',
-            'releases': [1, 2, 1, 0, 0]
+            'releases': [1, 2, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1]
         }
     ])
     .controller('office', function ($scope, OfficeConfig, OfficeConfig) {
--- src/office/ui/desk-directive.js
+++ src/office/ui/desk-directive.js
@@ -1,4 +1,11 @@
 angular.module('office.ui', [])
+    .service('RandomNum', function () {
+        this.clamp = function (min, max) {
+            var max = max - min + 1;
+
+            return Math.floor((Math.random() * max) + min);
+        };
+    })
     .directive('desk', function () {
         return {
             restrict: 'E',
@@ -9,36 +16,48 @@ angular.module('office.ui', [])
                 developer: '='
             },
 
-            controller: function ($scope, $element, $timeout, DeskHelper) {
+            controller: function ($scope, $element, $timeout, DeskHelper, RandomNum) {
                 var heatmap,
                     canvas = angular.element($element[0].querySelector('.heatmap')),
 
-                    heatData = {
-                        x: 200,
-                        y: 200,
-                        intensity: 0.5
+                    createSize = function (n) {
+                        return n * 100;
                     },
 
-                    adjustData = function (dev) {
+                    /**
+                     * @return {array} The array of releases.
+                     */
+                    createData = function (dev) {
                         // This is where the business logic needs to happen
-                        heatData.size = 100 || dev.releases[0];
+                        var rdata = [];
+
+                        dev.releases.forEach(function (release) {
+                            rdata.push({
+                                x: RandomNum.clamp(50, $element[0].clientWidth - 50),
+                                y: RandomNum.clamp(50, $element[0].clientHeight - 50),
+                                size: createSize(release),
+                                intensity: 0.5
+                            });
+                        });
+
+                        return rdata;
                     },
 
                     /**
                      * @param {null|object} dev The data for this desk.
                      */
                     init = function () {
-                        var dev = $scope.developer;
+                        var dev = $scope.developer,
+                            heatData;
 
                         if (dev) {
-                            adjustData(dev);
-
+                            heatData = createData(dev);
                             console.log(heatData);
 
                             $timeout(function () {
                                 DeskHelper.getPos($element);
 
-                                heatmap.addPoints([heatData]);
+                                heatmap.addPoints(heatData);
                                 heatmap.update();
                                 heatmap.display();
                             });
